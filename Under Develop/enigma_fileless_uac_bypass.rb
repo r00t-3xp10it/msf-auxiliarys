43aebf8e1d63133f5d50df1b161431bf06d4d854
root <root@localhost.localdomain>
2017-01-02 14:37:58 +0000

    local RCE privilege escalation

---
+++ Under Develop/enigma_fileless_uac_bypass.rb
@@ -0,0 +1,13 @@
+##
+# This file is part of the Metasploit Framework and may be subject to
+# redistribution and commercial restrictions. Please see the Metasploit
+# Framework web site for more information on licensing and terms of use.
+# http://metasploit.com/framework/
+##
+
+##
+# enigma_fileless_uac_bypass.rb
+# $Id$ 1.0 Author: r00t-3xp10it | SSA RedTeam @2016
+# Credits: enigma + mattifestation
+# POC: https://enigma0x3.net/2016/08/15/fileless-uac-bypass-using-eventvwr-exe-and-registry-hijacking/
+##
